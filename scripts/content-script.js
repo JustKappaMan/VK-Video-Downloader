3dd5e8f1331adaccddf51e6b4e27b35350ad22fc
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-01-26 18:53:17 +0500

    Release v1.0.2
    * You no longer need to click the extension icon to see download links. They'll appear automatically.
    * If a video is embedded, information rather than download links will appear. No more JS alerts.
    * Removed service worker
    * Removed unnecessary permissions
    * Massive refactoring
    * Updated README

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,66 +1,97 @@
 'use strict';
 
-if (
-  location.hostname === 'm.vk.com' &&
-  document.querySelector('div.VideoPage__video iframe')
-) {
-  alert('Видео встроено со стороннего сайта. Воспользуйтесь инструментами для скачивания с исходного сайта.');
-} else if (
-  location.hostname === 'm.vk.com' &&
-  document.querySelector('div.VideoPage__video video.vv_inline_video') &&
-  !document.querySelector('#downloadPanel')
-) {
-  document
-    .querySelector('div.VideoPage__video')
-    .after(createDownloadPanel(getSources()));
-}
+let lastUrl = location.href;
 
+new MutationObserver(() => {
+  if (location.href !== lastUrl) {
+    lastUrl = location.href;
+  }
+  if (location.pathname.includes('/video-')) {
+    const interval = setInterval(() => {
+      if (document.querySelector('div.VideoPage__video')) {
+        clearInterval(interval);
+        main();
+      }
+    }, 100);
+  }
+}).observe(document, { subtree: true, childList: true });
 
-function getSources() {
-  const sourceTags = document.querySelectorAll(
-    'video.vv_inline_video source[type="video/mp4"]'
-  );
-  let sources = {};
+function main() {
+  if (
+    document.querySelector('div.VideoPage__video iframe') &&
+    !document.querySelector('#vkVideoDownloaderPanel')
+  ) {
+    showPanel(createErrorPanel());
+  } else if (
+    document.querySelector('div.VideoPage__video video') &&
+    !document.querySelector('#vkVideoDownloaderPanel')
+  ) {
+    showPanel(createDownloadPanel(getSources()));
+  }
 
-  for (const tag of sourceTags) {
-    if (tag.src.includes('&type=4')) {
-      // Да, 144p выбивается из общей логики и имеет тип 4.
-      // Возможно отглоски какого-то легаси.
-      sources['144p'] = tag.src;
-    } else if (tag.src.includes('&type=0')) {
-      sources['240p'] = tag.src;
-    } else if (tag.src.includes('&type=1')) {
-      sources['360p'] = tag.src;
-    } else if (tag.src.includes('&type=2')) {
-      sources['480p'] = tag.src;
-    } else if (tag.src.includes('&type=3')) {
-      sources['720p'] = tag.src;
-    } else if (tag.src.includes('&type=5')) {
-      sources['1080p'] = tag.src;
+  function getSources() {
+    const sourceTags = document.querySelectorAll(
+      'video source[type="video/mp4"]'
+    );
+    let sources = {};
+
+    for (const tag of sourceTags) {
+      if (tag.src.includes('&type=4')) {
+        // Да, 144p выбивается из общей логики и имеет тип 4.
+        // Возможно отглоски какого-то легаси.
+        sources['144p'] = tag.src;
+      } else if (tag.src.includes('&type=0')) {
+        sources['240p'] = tag.src;
+      } else if (tag.src.includes('&type=1')) {
+        sources['360p'] = tag.src;
+      } else if (tag.src.includes('&type=2')) {
+        sources['480p'] = tag.src;
+      } else if (tag.src.includes('&type=3')) {
+        sources['720p'] = tag.src;
+      } else if (tag.src.includes('&type=5')) {
+        sources['1080p'] = tag.src;
+      }
     }
+
+    return sources;
   }
 
-  return sources;
-}
+  function createDownloadPanel(sources) {
+    const label = document.createElement('span');
+    label.innerText = 'Скачать:';
+    label.style.margin = '0 2px 0 0';
 
+    let panel = document.createElement('div');
+    panel.id = 'vkVideoDownloaderPanel';
+    panel.style.margin = '8px 12px';
+    panel.appendChild(label);
 
-function createDownloadPanel(sources) {
-  let panel = document.createElement('div');
-  panel.id = 'downloadPanel';
-  panel.style.margin = '8px 12px';
+    for (const [quality, url] of Object.entries(sources)) {
+      let aTag = document.createElement('a');
+      aTag.href = url;
+      aTag.innerText = quality;
+      aTag.style.margin = '0 2px';
+      panel.appendChild(aTag);
+    }
 
-  let label = document.createElement('span');
-  label.innerText = 'Скачать:';
-  label.style.margin = '0 2px 0 0';
-  panel.appendChild(label);
-
-  for (const [quality, url] of Object.entries(sources)) {
-    let aTag = document.createElement('a');
-    aTag.href = url;
-    aTag.innerText = quality;
-    aTag.style.margin = '0 2px';
-    panel.appendChild(aTag);
+    return panel;
   }
 
-  return panel;
+  function createErrorPanel() {
+    const label = document.createElement('span');
+    label.innerText = 'Видео со стороннего сайта. Воспользуйтесь инструментами для скачивания с исходного сайта.';
+    label.style.color = '#f00';
+    label.style.margin = '0 2px 0 0';
+
+    let panel = document.createElement('div');
+    panel.id = 'vkVideoDownloaderPanel';
+    panel.style.margin = '8px 12px';
+    panel.appendChild(label);
+
+    return panel;
+  }
+
+  function showPanel(panel) {
+    document.querySelector('div.VideoPage__video').after(panel);
+  }
 }
--- service-worker.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-chrome.action.onClicked.addListener(async (tab) => {
-  await chrome.scripting.executeScript({
-    target: {tabId: tab.id},
-    files: ['scripts/content-script.js'],
-  });
-});
