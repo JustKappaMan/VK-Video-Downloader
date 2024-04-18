62ff3783f24e2c479fefe5abe28dbd5896f05161
Kirill Volozhanin <volozhaninkirill@gmail.com>
2024-04-18 18:30:23 +0500

    Update for chromium mobile version

--- chromium/scripts/mobile.js
+++ chromium/scripts/mobile.js
@@ -45,7 +45,7 @@ function showDownloadPanel() {
 
 function showErrorPanel() {
   const label = document.createElement('span');
-  label.innerText = 'Видео со стороннего сайта. Воспользуйтесь инструментами для скачивания с исходного сайта.';
+  label.innerText = 'Видео со стороннего сайта. Воспользуйтесь инструментами для скачивания с него.';
   label.style.color = '#f00';
 
   const panel = document.createElement('div');
